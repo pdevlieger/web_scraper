a846d8364fbc34f1080aa21c0c32ade481f9412b
uctpphd <pieterdevlieger@gmail.com>
2012-12-10 23:47:20 -0500

    just another update

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template
 from pymongo import Connection
 
 app = Flask(__name__)
 
-connection = Connection()
+mongodb_uri = 'mongodb://localhost:27017'
+db_name = 'mongoquest'
+try:
+	connection = Connection(mongodb_uri)
+	database = connection[db_name]
+except:
+	print('Error')
+	connection = None
+
+#connection = Connection()
 db = connection.test
 collection_1 = db.footballparser
 collection_2 = db.footballpermanent
@@ -17,4 +26,16 @@ def player_data(name):
 	return render_template('%s.html' % name)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	if connection:
+		app.run(debug=True)
+
+#	mongodb_uri = 'mongodb://localhost:27017'
+#	db_name = 'mongoquest'
+#	try:
+#		connection = Connection(mongodb_uri)
+#		database = connection[db_name]
+#	except:
+#		print('Error: unable to connect to database. ')
+#		connection = None
+#	if connection:
+#		app.run
