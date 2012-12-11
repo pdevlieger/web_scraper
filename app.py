e4e96cfac655fcd2847b0f3594ba3c63ce9aed79
uctpphd <pieterdevlieger@gmail.com>
2012-12-11 00:52:08 -0500

    init

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template
 from pymongo import Connection
+#import os
+
+#mongo_url = os.getenv('MONGOLAB_URI','mongodb://localhost:27017')
 
 app = Flask(__name__)
 
-mongodb_uri = 'mongodb://localhost:27017'
-db_name = 'mongoquest'
-try:
-	connection = Connection(mongodb_uri)
-	database = connection[db_name]
-except:
-	print('Error')
-	connection = None
-
-#connection = Connection()
+connection = Connection('mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363')
 db = connection.test
 collection_1 = db.footballparser
 collection_2 = db.footballpermanent
@@ -29,13 +23,5 @@ if __name__ == '__main__':
 	if connection:
 		app.run(debug=True)
 
-#	mongodb_uri = 'mongodb://localhost:27017'
-#	db_name = 'mongoquest'
-#	try:
-#		connection = Connection(mongodb_uri)
-#		database = connection[db_name]
-#	except:
-#		print('Error: unable to connect to database. ')
-#		connection = None
-#	if connection:
-#		app.run
+
+# mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363
