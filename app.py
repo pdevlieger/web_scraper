a3732af662bd646a090a882abda6d232b098a3c0
uctpphd <pieterdevlieger@gmail.com>
2012-12-11 02:33:23 -0500

    updated port-checks

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from pymongo import Connection
-#import os
+import os
 
 #mongo_url = os.getenv('MONGOLAB_URI','mongodb://localhost:27017')
 
 app = Flask(__name__)
 
-connection = Connection('mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363')
+port = int(os.environ.get('PORT', 5000))
+
+connection = Connection()
 db = connection.test
 collection_1 = db.footballparser
 collection_2 = db.footballpermanent
@@ -21,7 +23,7 @@ def player_data(name):
 
 if __name__ == '__main__':
 	if connection:
-		app.run(debug=True)
+		app.run(host='0.0.0.0', port = port)
 
 
 # mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363
