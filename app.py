3719c1b65046407a7f260ae74948e6b59c514265
uctpphd <pieterdevlieger@gmail.com>
2012-12-12 17:36:03 -0500

    check whether everything works...

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
 from flask import Flask, render_template
 from pymongo import Connection
+from urlparse import urlsplit
 import os
 
-list_names = ['Moussa Dembele', 'Marouane Fellaini','Vincent Kompany','Romelu Lukaku',
-'Simon Mignolet','Thomas Vermaelen','Jan Vertonghen']
-
-#mongo_url = os.getenv('MONGOLAB_URI','mongodb://localhost:27017')
-
-app = Flask(__name__)#, instance_relative_config=True)
-
+url = os.getenv('MONGOLAB_URI','mongodb://localhost:27017/test')
+parsed = urlsplit(url)
+db_name = parsed.path[1:]
 port = int(os.environ.get('PORT', 5000))
+db = Connection(url)[db_name]
 
-connection = Connection('mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363')
-#connection = Connection('mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363')
-#connection = Connection()
-db = connection.test
-collection_1 = db.footballparser
-collection_2 = db.footballpermanent
-db.authenticate('heroku_app9943363','703781')
-
-def get_name_stats(player):
-	dictionary_permanent = db.footballpermanent.find()
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -28,28 +17,28 @@ def index():
 
 @app.route('/<player>')
 def player_data(player):
-#	permanent_stats = [x for x in collection_2.find({'name': player})]
-#	match_stats = [x for x in db.footballparser.find({'name': player})]
-#	last_match = [x for x in match_stats if x['url']!='n/a'][-1]
-#	next_match = [x for x in match_stats if x['url']=='n/a'][0]
-	return render_template('template.html', name = player, dictionary_perm = {'Final rating': 4, 'Attacking': 5, 'Defending': 7},
-#	home_team = last_match['home team'], score = last_match['score'], away_team = last_match['away team'],
-#	dictionary_match = last_match, home_team_next = next_match['home team'], away_team_next = next_match['away team'])
-	home_team = 'Manchester City', score = '3 - 1', away_team = 'Arsenal',
-	dictionary_match = {'Goals': 0, 'Yellow card': 1, 'Man of the Match': True}, home_team_next = 'Sunderland', away_team_next = 'Manchester City')
-
-#name = 'Vincent Kompany',
-#	dictionary_perm = {'Final rating': 4, 'Attacking': 5, 'Defending': 7},
-#	home_team = 'Manchester City',
-#	score = '3 - 1',
-#	away_team = 'Arsenal',
-#	dictionary_match = {'Goals': 0, 'Yellow card': 1, 'Man of the Match': True},
-#	home_team_next = 'Sunderland',
-#	away_team_next = 'Manchester City'	
+	permanent_stats = [x for x in db.footballpermanent.find({'name': player})][0]
+	match_stats = [x for x in db.footballparser.find({'name': player})]
+	last_match = [x for x in match_stats if x['url']!='n/a'][-1]
+	next_match = [x for x in match_stats if x['url']=='n/a'][0]
+	# template.html has a number of inputs:
+	# => name: just steal the player name from the url and pass it
+	# => dictionary_perm: a dict that holds the more permanent over-all-season stats
+	# => home_team: the home team of the match last played
+	# => score: the score of the match last played
+	# => away_team: the way team of the match last played
+	# => dictionary_match: a dict that holds the stats of the last match played
+	# => home_team_next: home team for the next match
+	# => away_team_next: away team for the next match
+	return render_template('template.html', name = player, dictionary_perm = permanent_stats,
+	home_team = last_match['home team'], score = last_match['score'], away_team = last_match['away team'],
+	dictionary_match = last_match, home_team_next = next_match['home team'], away_team_next = next_match['away team'])
 
 if __name__ == '__main__':
-	if connection:
-#		app.run(debug=True)
+	print url
+	if '@' in 'mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363':
+		print [x for x in db.footballpermanent.find({'name': 'Jan Vertonghen'})][0]
+		user_pass = parsed.netloc.split('@')[0].split(':')
+		db.authenticate(user_pass[0], user_pass[1])
+		print [x for x in db.footballpermanent.find({'name': 'Jan Vertonghen'})][0]
 		app.run(host='0.0.0.0', port = port, debug=True)
-
-# mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363
