71d02b68afb5957fd52f02f810e63efdebf4e4ea
Thomas Ballinger <thomasballinger@gmail.com>
2012-12-17 17:44:07 -0500

    simplfy template

--- app.py
+++ app.py
@@ -15,18 +15,13 @@ def player_data(player):
     match_stats = [x for x in db.footballparser.find({'name': player})]
     last_match = [x for x in match_stats if x['url']!='n/a'][-1]
     next_match = [x for x in match_stats if x['url']=='n/a'][0]
-    # template.html has a number of inputs:
-    # => name: just steal the player name from the url and pass it
-    # => dictionary_perm: a dict that holds the more permanent over-all-season stats
-    # => home_team: the home team of the match last played
-    # => score: the score of the match last played
-    # => away_team: the way team of the match last played
-    # => dictionary_match: a dict that holds the stats of the last match played
-    # => home_team_next: home team for the next match
-    # => away_team_next: away team for the next match
-    return render_template('template.html', name = player, dictionary_perm = permanent_stats,
-    home_team = last_match['home team'], score = last_match['score'], away_team = last_match['away team'],
-    dictionary_match = last_match, home_team_next = next_match['home team'], away_team_next = next_match['away team'])
+    template_data = {
+            'name' : player,
+            'stats' : permanent_stats,
+            'last_match' : last_match,
+            'next_match' : next_match,
+            }
+    return render_template('template.html', **template_data)
 
 if __name__ == '__main__':
 #    url = 'mongodb://heroku_app9943363:ltoo03cli1dnufi04kepkljv4l@ds045147.mongolab.com:45147/heroku_app9943363'
