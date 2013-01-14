25aef0029470f309d1f28026fc029f72cb8b6ec9
uctpphd <pieterdevlieger@gmail.com>
2013-01-14 10:35:09 -0800

    added list to iterate over players

--- data_consolidate.py
+++ data_consolidate.py
@@ -2,6 +2,12 @@ from database_static import static_data
 from database_matches import get_player_match_stats, get_calendar_for_player
 import dblib
 
+player_list = ['C. Benteke','Moussa Dembele','Marouane Fellaini',
+    'Eden Hazard','Vincent Kompany','Romelu Lukaku','Simon Mignolet','Kevin Mirallas',
+    'Thomas Vermaelen','Jan Vertonghen','Kevin De Bruyne','Igor De Camargo',
+    'Timmy Simmons','Daniel Van Buyten','Thibaut Courtois','Jean-Francois Gillet',
+    'Gaby Mudingayi','Radja Nainggolan','Toby Alderweireld']
+
 def set_perm_scores(name):
     return static_data(name)
 
@@ -16,7 +22,7 @@ def make_database():
     db = dblib.get_connection()
     db.footballparser.remove()
     db.footballpermanent.remove()
-    for name in url_by_player.keys():
+    for name in player_list:
         db.footballparser.insert(set_calendar_stats(name))
         db.footballpermanent.insert(set_perm_scores(name))
 
